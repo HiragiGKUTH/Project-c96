024e378a48acad1f759638f5f6f930ef67dd3911
Shimaoka-Shuya <hiragi.gkuth.dev@gmail.com>
2019-05-16 23:33:53 +0900

    A: BUllet class

--- siv3d-project/project/c96/src/Bullet.cpp
+++ siv3d-project/project/c96/src/Bullet.cpp
@@ -1,9 +1,23 @@
-//
-//  Bullet.cpp
-//  c96
-//
-//  Created by 嶌岡柊也 on 2019/05/16.
-//  Copyright © 2019 gkuth. All rights reserved.
-//
-
 #include "Bullet.hpp"
+
+Bullet::Bullet(Vec2 pos, double speed, double ang) {
+    this->pos = pos;
+    this->speed = speed;
+    this->ang = ang;
+    
+    // 右を初期値に回す
+    this->vel = Vec2::Right().rotate(ang)*speed;
+}
+
+bool Bullet::update() {
+    vel = Vec2::Right().rotate(ang)*speed;
+    pos.moveBy(vel);
+    collision.setPos(pos);
+    
+    return Window::ClientRect().intersects(collision);
+}
+
+void Bullet::draw() const {
+    //todo: draw actual bullet
+    collision.draw();
+}
--- siv3d-project/project/c96/src/Bullet.hpp
+++ siv3d-project/project/c96/src/Bullet.hpp
@@ -1,14 +1,28 @@
-//
-//  Bullet.hpp
-//  c96
-//
-//  Created by 嶌岡柊也 on 2019/05/16.
-//  Copyright © 2019 gkuth. All rights reserved.
-//
-
 #ifndef Bullet_hpp
 #define Bullet_hpp
 
-#include <stdio.h>
+#include <Siv3D.hpp>
+#include "Task.hpp"
+
+class Bullet : public Task {
+private:
+    Vec2 pos;
+    Vec2 vel;
+    double speed;
+    double ang;
+    unsigned int cnt;
+    Circle collision;
+    
+public:
+    Bullet(Vec2 pos, double speed, double ang);
+    
+    bool update() override;
+    void draw() const override;
+    
+    void setAngle(double ang);
+    void setSpeed(double speed);
+    
+    void activateGravity(double strength, double direction);
+};
 
 #endif /* Bullet_hpp */

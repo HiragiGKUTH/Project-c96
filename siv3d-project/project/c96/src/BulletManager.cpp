3948ed0651d4cd68ceb512fbe01d7cdffa634f8e
Shimaoka-Shuya <hiragi.gkuth.dev@gmail.com>
2019-05-24 13:16:02 +0900

    A: Bullet Manager

--- siv3d-project/project/c96/src/BulletManager.cpp
+++ siv3d-project/project/c96/src/BulletManager.cpp
@@ -1,9 +1,41 @@
-//
-//  BulletManager.cpp
-//  c96
-//
-//  Created by 嶌岡柊也 on 2019/05/24.
-//  Copyright © 2019 gkuth. All rights reserved.
-//
-
 #include "BulletManager.hpp"
+
+BulletManager::BulletManager() {
+    
+}
+
+bool BulletManager::add(Vec2 pos, double speed, double ang) {
+    if (bulletList.size() >= MAX_BULLETS) {
+        Print << U"BulletManager: can't add more bullet. >10000";
+        return false;
+    }
+    // make bullet and push
+    bulletList.push_back(std::make_shared<Bullet>(pos, speed, ang));
+    return true;
+}
+
+unsigned long BulletManager::getBulletNum() {
+    return bulletList.size();
+}
+
+std::shared_ptr<Bullet> BulletManager::at(int index) {
+    return bulletList.at(index);
+}
+
+bool BulletManager::update() {
+    unsigned long bulletNum = bulletList.size();
+    for (int i = 0; i < bulletNum; ++i) {
+        if (!bulletList.at(i)->update()) {
+            bulletList.remove_at(i);
+            bulletNum--;
+        }
+    }
+    return true;
+}
+
+void BulletManager::draw() const {
+    Print << U"BulletManager: " << bulletList.size() << U"bullets";
+    for (auto bullet : bulletList){
+        bullet->draw();
+    }
+}
--- siv3d-project/project/c96/src/BulletManager.hpp
+++ siv3d-project/project/c96/src/BulletManager.hpp
@@ -1,14 +1,27 @@
-//
-//  BulletManager.hpp
-//  c96
-//
-//  Created by 嶌岡柊也 on 2019/05/24.
-//  Copyright © 2019 gkuth. All rights reserved.
-//
-
 #ifndef BulletManager_hpp
 #define BulletManager_hpp
 
-#include <stdio.h>
+#include "Siv3D.hpp"
+#include <memory>
+
+#include "Bullet.hpp"
+
+// Enemy has this Manager
+class BulletManager : public Task {
+private:
+    Array<std::shared_ptr<Bullet>> bulletList;
+    const unsigned int MAX_BULLETS = 10000;
+    
+public:
+    BulletManager();
+    
+    bool add(Vec2 pos, double speed, double ang);
+    
+    unsigned long getBulletNum();
+    std::shared_ptr<Bullet> at(int index);
+    
+    bool update() override;
+    void draw() const override;
+};
 
 #endif /* BulletManager_hpp */

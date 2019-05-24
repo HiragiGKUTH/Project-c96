0464e582692e9fb622591e36e34cef38512c7799
Shimaoka-Shuya <hiragi.gkuth.dev@gmail.com>
2019-05-24 13:15:11 +0900

    A: emply BulletManager

---
+++ siv3d-project/project/c96/src/BulletManager.cpp
@@ -0,0 +1,9 @@
+//
+//  BulletManager.cpp
+//  c96
+//
+//  Created by 嶌岡柊也 on 2019/05/24.
+//  Copyright © 2019 gkuth. All rights reserved.
+//
+
+#include "BulletManager.hpp"
---
+++ siv3d-project/project/c96/src/BulletManager.hpp
@@ -0,0 +1,14 @@
+//
+//  BulletManager.hpp
+//  c96
+//
+//  Created by 嶌岡柊也 on 2019/05/24.
+//  Copyright © 2019 gkuth. All rights reserved.
+//
+
+#ifndef BulletManager_hpp
+#define BulletManager_hpp
+
+#include <stdio.h>
+
+#endif /* BulletManager_hpp */
